fb9c4dcf9abb17ba9c68a4901fcc75f18c14ed74
Harry Lachenmayer <harrylachenmayer@gmail.com>
2017-12-16 19:49:42 +0000

    Optionally read config file path from an argument.

--- index.js
+++ index.js
@@ -1,19 +1,31 @@
 #!/usr/bin/env node
-
-var manager = require('./lib/manager')
+const os = require('os')
+const path = require('path')
+const manager = require('./lib/manager')
 
 run()
 function run () {
-  if (process.argv[2]) {
+  let configPath
+  if (process.argv.length === 3) {
+    configPath = process.argv[2]
+  } else if (process.argv.length === 2) {
+    configPath = process.env.DATHTTPD_CONFIG || path.join(os.homedir(), '.dathttpd.yml')
+  } else {
     return usage()
   }
 
   // start server
-  manager.start()
+  manager.start(configPath)
 }
 
 function usage () {
-  console.log(`dathttpd - starts the server
-Env Vars:
+  console.log(`Usage: dathttpd [<config>]
+Starts a server with a config file at the path <config>.
+
+If no path is given, the path to the config is looked up in the
+DATHTTPD_CONFIG environment variable. If this is not set, the config
+will be read from the default path ~/.dathttpd.yml.
+
+Env vars:
    DATHTTPD_CONFIG=~/dathttpd.yml - location of the config file`)
 }
