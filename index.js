b8093b50d8772db04276c078e7c91397f3c257f5
Harry Lachenmayer <harrylachenmayer@gmail.com>
2017-12-30 17:00:48 +0000

    Make config file option a named argument.

--- index.js
+++ index.js
@@ -3,29 +3,19 @@ const os = require('os')
 const path = require('path')
 const manager = require('./lib/manager')
 
+const defaultConfigPath = process.env.DATHTTPD_CONFIG || path.join(os.homedir(), '.dathttpd.yml')
+
+const argv = require('yargs')
+  .usage('dathttpd - Start a dathttpd server')
+  .option('config', {
+    describe: 'Path to the config file. If no path is given, the path to the config is looked up in the DATHTTPD_CONFIG environment variable. If this is not set, the config will be read from the default path ~/.dathttpd.yml.',
+    default: defaultConfigPath,
+  })
+  .argv
+
 run()
 function run () {
-  let configPath
-  if (process.argv.length === 3) {
-    configPath = process.argv[2]
-  } else if (process.argv.length === 2) {
-    configPath = process.env.DATHTTPD_CONFIG || path.join(os.homedir(), '.dathttpd.yml')
-  } else {
-    return usage()
-  }
-
+  const configPath = argv.config
   // start server
   manager.start(configPath)
-}
-
-function usage () {
-  console.log(`Usage: dathttpd [<config>]
-Starts a server with a config file at the path <config>.
-
-If no path is given, the path to the config is looked up in the
-DATHTTPD_CONFIG environment variable. If this is not set, the config
-will be read from the default path ~/.dathttpd.yml.
-
-Env vars:
-   DATHTTPD_CONFIG=~/dathttpd.yml - location of the config file`)
 }
